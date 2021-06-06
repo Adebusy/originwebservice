aec85e10dffb3a720a51a7f8124eacdf21aa74e2
ALAO RAMON ADEBISI <alaonr@MacBooks-MacBook-Pro.local>
2021-06-06 22:01:32 +0100

    structure update

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	route := gin.Default()
+	route.GET("/", AppStarted)
+	route.Run("8016")
+
 }
+
+func AppStarted(c *gin.Context) {
+	c.JSON(http.StatusOK, "Application is up.")
+
+}
+
+//if err := ctx.ShouldBindJSON(&visitorRequestBody); err != nil {
+//	httputil.NewError(ctx, http.StatusBadRequest, err)
+//return
+//}
+//r.GET("/user/:name", func(c *gin.Context) {
+//	user := c.Params.ByName("name")
+//value, ok := db[user]
+//if ok {
+//	c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+//} else {
+//	c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+//}
+//})
